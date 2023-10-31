d4ca7a8c015bf6014273d0a1384cce501d7bd413
mykbgwl <rockmb972@gmail.com>
2023-10-31 20:12:31 +0530

    first commit

---
+++ src/components/StartGame.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+import {Button} from "../styled/Button"
+
+const StartGame = ({toggle}) => {
+  return (
+    <Container>
+        <div>
+            <img src="./images/dices.png" alt="dice image" />
+        </div>
+        <div className="content">
+            <h1>DICE GAME</h1>
+            <Button onClick={toggle}>Play Now</Button>
+        </div>
+    </Container>
+  )
+}
+
+export default StartGame
+
+const Container = styled.div`
+    max-width: 1180px;
+    display: flex;
+    height: 100vh;
+    align-items: center;
+    margin : 0 auto;
+
+.content h1{
+    font-size: 96px;
+    white-space: nowrap;
+}
+
+`;
